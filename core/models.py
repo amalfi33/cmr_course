b5321afcf3f904460d292cc783f5fca3280cd786
amalfi33 <amalfiamalalena1210@gmail.com>
2024-02-07 23:05:40 +0300

    Models.Employee

--- core/models.py
+++ core/models.py
@@ -3,37 +3,62 @@ from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Курс обучения')
+    price = models.DecimalField(max_digits=10, decimal_places=3, verbose_name='Цена')
+
+    class Meta:
+        verbose_name = 'Курс'  
+        verbose_name_plural = 'Курсы'
+
+    def __str__(self):
+        return self.name
+
 class Transaction(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
-    # Дополнительные поля и методы
+
 
 class Attendance(models.Model):
     date = models.DateField()
-    # Дополнительные поля и методы
+
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # Дополнительные поля и методы
+
 
 class Employee(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # Дополнительные поля и методы
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    phone_number = models.CharField(max_length=20, verbose_name='Номер телефона')
+    position = models.CharField(max_length=50, verbose_name='Должность')
+    courses = models.ManyToManyField(Course, verbose_name='Курсы обучения')
+    salary = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Зарплата')
+
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.position}"
 
 class Teacher(Employee):
     speciality = models.CharField(max_length=100)
-    # Дополнительные поля и методы
 
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-    # Дополнительные поля и методы
 
 class Group(models.Model):
     name = models.CharField(max_length=100)
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Course, on_delete=models.CASCADE)
     students = models.ManyToManyField(Student)
-    # Дополнительные поля и методы
+
+    class Meta:
+        verbose_name = 'Группа'  
+        verbose_name_plural = 'Группы'
+
+    def __str__(self):
+        return self.name
+
 
 
 
