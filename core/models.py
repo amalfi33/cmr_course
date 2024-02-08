ccb62b3a348bd8484c16a8f05043b452fb4f9f76
amalfi33 <amalfiamalalena1210@gmail.com>
2024-02-08 18:41:25 +0500

    add models

--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+
 class Course(models.Model):
     name = models.CharField(max_length=100, verbose_name='Курс обучения')
     price = models.DecimalField(max_digits=10, decimal_places=3, verbose_name='Цена')
@@ -13,6 +14,22 @@ class Course(models.Model):
 
     def __str__(self):
         return self.name
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    phone_number = models.CharField(max_length=20, verbose_name='Номер телефона')
+    position = models.CharField(max_length=50, verbose_name='Должность')
+    courses = models.ManyToManyField(Course, verbose_name='Курс обучения')
+    salary = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Зарплата')
+
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.position}"
+
 
 class Transaction(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
@@ -26,21 +43,6 @@ class Attendance(models.Model):
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-
-class Employee(models.Model):
-    first_name = models.CharField(max_length=50, verbose_name='Имя')
-    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
-    phone_number = models.CharField(max_length=20, verbose_name='Номер телефона')
-    position = models.CharField(max_length=50, verbose_name='Должность')
-    courses = models.ManyToManyField(Course, verbose_name='Курсы обучения')
-    salary = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Зарплата')
-
-    class Meta:
-        verbose_name = 'Сотрудник'
-        verbose_name_plural = 'Сотрудники'
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name} - {self.position}"
 
 class Teacher(Employee):
     speciality = models.CharField(max_length=100)
