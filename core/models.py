4a871abc3d81da928eaaa766fd186e8d03382176
amalfi33 <amalfiamalalena1210@gmail.com>
2024-02-08 19:43:11 +0500

    view

--- core/models.py
+++ core/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 class Course(models.Model):
     name = models.CharField(max_length=100, verbose_name='Курс обучения')
     price = models.DecimalField(max_digits=10, decimal_places=3, verbose_name='Цена')
+    date_start = models.DateField()
 
     class Meta:
         verbose_name = 'Курс'  
@@ -16,7 +17,7 @@ class Course(models.Model):
         return self.name
 
 class Employee(models.Model):
-    user = models.OneToOneField(User)
+    user = models.ManyToManyField(User)
     phone_number = models.CharField(max_length=20, verbose_name='Номер телефона')
     position = models.CharField(max_length=50, verbose_name='Должность')
     courses = models.ManyToManyField(Course, verbose_name='Курс обучения')
@@ -39,7 +40,7 @@ class Transaction(models.Model):
 
 class Attendance(models.Model):
     date = models.DateField()
-    student = 
+    students =
 
 
 class Student(models.Model):
@@ -51,6 +52,8 @@ class Student(models.Model):
     course = models.ManyToManyField(Course, related_name= 'students')
     status = models.IntegerField(choices=StudentStatus.choices, default=1)
     phone = models.CharField(max_length=255)
+    code = models.CharField(max_length=8, null = True)
+    qr = models.ImageField(upload_to= 'students_qr/', null=True)
 
 
 class Teacher(Employee):
@@ -70,9 +73,5 @@ class Group(models.Model):
 
     def __str__(self):
         return self.name
+
     
-class User
-
-
-
-
