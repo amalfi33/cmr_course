a43b156221a400d2dcea5d6c01c67d1c372f5c29
amalfi33 <amalfiamalalena1210@gmail.com>
2024-02-10 20:34:10 +0300

    views.borya

---
+++ core/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Course, Student, Teacher, Employee
+from core.models import Employee 
+from core.models import Teacher
+
+
+# Курсы форма
+class CourseForm(forms.ModelForm):
+    teacher = forms.ModelChoiceField(queryset=Teacher.objects.all())
+    employee = forms.ModelChoiceField(queryset=Employee.objects.all())  # Замените на имя поля, которое вы хотите использовать
+
+
+    class Meta:
+        model = Course
+        fields = ['name', 'price','date_start','date_end', 'teacher', 'employee']
+
+
+# Учитель форма
+class TeacherForm(forms.ModelForm):
+    courses_taught = forms.ModelMultipleChoiceField(queryset=Course.objects.all(), required=False)
+
+    class Meta:
+        model = Teacher
+        fields = ['courses_taught']
+
